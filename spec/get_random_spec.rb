21ba472552551d9bd5268dab08f8a6750428a7e5
Gaelan D'costa <gdcosta@gmail.com>
2012-02-13 01:37:46 -0500

    Added another unit test which we have to consider, when we request more loans than redis contains

--- spec/get_random_spec.rb
+++ spec/get_random_spec.rb
@@ -20,4 +20,21 @@ describe 'Grabbing N random feeds' do
 
     get '/random/' + LOANS_TO_GRAB.to_s
   end
+
+  it "should return all redis feeds if we requested more loans than redis contains" do
+    LOANS_TO_GRAB = 20
+    LOANS_POPULATED = 10
+    redis = double("Redis instance")
+    Redis.should_receive(:new).and_return redis
+
+    # Since randomly choosing a key could produce duplicates, it's possible
+    # we had to discard some duplicate random selections.
+    redis.should_receive(:randomkey).at_least(LOANS_POPULATED).times.and_return do
+      Random.rand(LOANS_POPULATED)
+    end
+    redis.should_receive(:dbsize).and_return LOANS_POPULATED
+    redis.should_receive(:get).exactly(LOANS_POPULATED).times.and_return "{}"
+
+    get '/random/' + LOANS_TO_GRAB.to_s
+  end
 end
